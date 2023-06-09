b51405e58e9a3fc1260b7fd80fa42224d07660ca
hassan mohamed kamel <hassankamel755@gmail.com>
2023-06-10 02:57:21 +0300

    Add .env variables

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,14 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { ISignupForm } from '../pages/SignUp';
+// import React from 'react';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBGLQgY5uuA3zgpTUmFw8UaIgkgTeacIs8',
-  authDomain: 'react-firebase-demo-ba1ce.firebaseapp.com',
-  projectId: 'react-firebase-demo-ba1ce',
-  storageBucket: 'react-firebase-demo-ba1ce.appspot.com',
-  messagingSenderId: '561743685940',
-  appId: '1:561743685940:web:33bc6779ada75891d75d61',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
